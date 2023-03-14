769bd9f1bc5b8a1347e92b5f297db93f23f0fc3c
Thevarpiran Sneha <thevarpiransneha@L-GXXMFWX9KH.local>
2023-03-14 12:27:09 +0530

    hello

--- Jenkins_pipeline/Jenkins.groovy
+++ Jenkins_pipeline/Jenkins.groovy
@@ -1,9 +1,40 @@
-pipeline  { 
+pipeline{
     agent { node { label "$env.label"}}
     stages{
-        stage('code checkout'){
+        stage('Code checkout'){
             steps{
-                git branch: 'main', 
+                git branch: 'main', credentialsId: 'Git_cred', url: 'https://github.com/sneha-theva/groovescriptjenkins'
+                echo 'code checkout'
+            }
+        }
+
+        stage('Create package'){
+            steps{
+                sh 'zip -r jenkins.zip index.html'
+            }
+        }
+
+        stage('EC2 Instance'){
+            steps{
+                sshagent(['snehanew']) {
+                    sh script:'''
+                            
+                            ssh -o StrictHostKeyChecking=no ec2-user@100.26.230.113 '
+                                sudo su
+                                sudo yum update -y
+                                sudo yum install httpd -y
+                                sudo service httpd start
+                                sudo chkconfig httpd on
+                                sudo cd /var/www/html
+                                sudo chmod 777 /var/www/html
+                            '
+                            scp jenkins.zip ec2-user@100.26.230.113:/var/www/html
+                            ssh ec2-user@100.26.230.113 '
+                                cd /var/www/html
+                                unzip jenkins.zip -d .
+                            '
+                            '''
+                }
             }
         }
     }
